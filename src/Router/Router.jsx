14cd47886f6166f323733f67e4339d6587b3e432
Ian Pelina <ianpelina42@gmail.com>
2023-11-04 14:22:53 +0100

    correction store reducers actions

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,13 +1,13 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "../pages/Home/Home";
-import SignIn from "../pages/SignIn/SignIn";
+import Login from "../pages/Login/Login";
 import User from "../pages/User/User";
 
 export default function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-in" element={<Login />} />
             <Route path="/user" element={<User />} />
         </Routes>
     )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+
 import App from './App';
 
 /**
@@ -17,9 +16,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
 );

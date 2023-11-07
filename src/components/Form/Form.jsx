2e6b2fb0c09b1c0f28dd83791ee6d1c91765e642
Ian Pelina <ianpelina42@gmail.com>
2023-11-07 13:49:07 +0100

    connexion-ok

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,51 @@
-import './Form.scss';
+import "./Form.scss";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
 
-import { Link } from 'react-router-dom';
+import { login } from "../../actions/login.action";
+import loginReducer from "../../reducers/login.reducer";
 
 export default function Form() {
-    return (
-        <form>
-            <div className="input-wrapper">
-                <label htmlFor="username">Username</label><input type="text" id="username" />
-            </div>
-            <div className="input-wrapper">
-                <label htmlFor="password">Password</label><input type="password" id="password" />
-            </div>
-            <div className="input-remember">
-                <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
-            </div>
-            <Link to="/user" className="sign-in-button">Sign In</Link>
-        </form>
-    )
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const error = useSelector(({ loginReducer }) => loginReducer.error);
+  const isLoggedIn = useSelector(state => state.loginReducer.isLoggedIn);
+  
+  function handleSubmit($event) {
+    $event.preventDefault();
+    try {
+        dispatch(login({ email: username, password }));
+      } catch (error) {
+        console.error('Login error:', error.message);
+      }
+  }
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/user');
+    }
+  }, [isLoggedIn, navigate]);
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <div className="input-wrapper">
+        <label htmlFor="username">Username</label><input type="text" id="username" onChange={({target}) => setUsername(target.value)} />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="password">Password</label><input type="password" id="password" onChange={({target}) => setPassword(target.value)} />
+      </div>
+      <div className="input-remember">
+        <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button type="submit" className="sign-in-button">Sign In</button>
+    </form>
+      { error && <div><p>{error}</p></div> }
+    </>
+  );
 }
--- src/reducers/login.reducer.js
+++ src/reducers/login.reducer.js
@@ -1,5 +1,40 @@
-const initialState = {};
+import { createSlice } from "@reduxjs/toolkit";
+import { login } from "../actions/login.action";
 
-export default function loginReducer(state=initialState, action) {
-    return state;
-}
+const initialState = {
+  isLoggedIn: false,  
+  token: localStorage.getItem('token'),
+  loading: false,
+  error: null,
+};
+
+const slice = createSlice({
+  name: "token",
+  initialState,
+  reducers: {
+    reset: (state, action) => {
+      state.token = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state, action) => {
+      state.loading = true;
+      state.error = null;
+      state.token = null;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.isLoggedIn = true;
+      localStorage.setItem('token', action.payload.token);
+      state.token = action.payload.token;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      console.error(action.error.message);
+      state.loading = false;
+      state.error = "Le mot de passe ou l'email est incorrect.";
+    });
+  }
+});
+
+export const { reset } = slice.actions;
+export default slice.reducer;
